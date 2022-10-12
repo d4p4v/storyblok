4b7c36874cc5a329fe830b77ea00cfb14189e7f9
Dalibor Pavlov <pavlov.dalibor@hotmail.com>
2022-10-12 10:10:50 +0200

    update

--- src/components/Wrapper.vue
+++ src/components/Wrapper.vue
@@ -1,25 +1,29 @@
 <template>
     <div
         v-editable
-        class="flex w-full items-center justify-center flex-col lg:flex-row"
+        class="flex w-full items-center justify-center"
         :style="{
             backgroundColor: `${blok.bg.color}`,
             height: `${blok.height}`,
         }"
     >
         <div
-            v-for="(item, index) in blok.content"
-            :style="{
-                textAlign: `${item.alignment}`,
-                color: `${item.foreground.color}`,
-                width: `${calculatedWidth}`,
-                whiteSpace: 'pre-wrap',
-                wordSpacing: 'length',
-            }"
-            :key="item.component + index"
-            class="flex items-center justify-center"
+            class="flex items-center justify-center w-4/5 flex-col lg:flex-row"
         >
-            <p class="mb-7 w-5/6 text-justify">{{ item.content }}</p>
+            <div
+                v-for="(item, index) in blok.content"
+                :style="{
+                    textAlign: `${item.alignment}`,
+                    color: `${item.foreground.color}`,
+                    width: `${calculatedWidth}`,
+                    whiteSpace: 'pre-wrap',
+                    wordSpacing: 'length',
+                }"
+                :key="item.component + index"
+                class="flex items-center justify-center"
+            >
+                <p class="mb-7 w-5/6 text-justify">{{ item.content }}</p>
+            </div>
         </div>
     </div>
 </template>
